0a8edd1e707da455181b987e026244689754c34f
Noah Cylich <noahcylich@gmail.com>
2026-01-04 17:05:20 -0800

    fixed import order

--- examples/bert/sft.py
+++ examples/bert/sft.py
@@ -40,6 +40,12 @@ from functools import partial
 # Import dllm FIRST to disable torch.compile on TPU before transformers loads
 import dllm
 
+# Disable warn_if_padding_and_no_attention_mask BEFORE importing transformers models
+# This check calls __contains__ on input_ids which forces TPU-to-host sync every forward pass
+if dllm.utils.device.is_tpu_available():
+    from transformers import modeling_utils as _modeling_utils
+    _modeling_utils.warn_if_padding_and_no_attention_mask = lambda *args, **kwargs: None
+
 import accelerate
 import transformers
 
@@ -84,13 +90,6 @@ def train():
 
     # ----- Model ------------------------------------------------------------------
     model = dllm.utils.get_model(model_args=model_args)
-
-    # Disable padding warning check on TPU - it forces device-to-host sync every forward pass
-    # by checking if pad_token_id is in input_ids (calls __contains__ which syncs)
-    if dllm.utils.device.is_tpu_available():
-        # Monkey-patch the warn_if_padding_and_no_attention_mask to be a no-op
-        from transformers import modeling_utils as _modeling_utils
-        _modeling_utils.warn_if_padding_and_no_attention_mask = lambda *args, **kwargs: None
     # ----- Tokenizer --------------------------------------------------------------
     tokenizer = dllm.utils.get_tokenizer(model_args=model_args)
 
